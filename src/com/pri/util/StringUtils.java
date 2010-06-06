27dc5546a5d51796d0fde8f488f90bed2d9265c3
Mike <gostev@ebi.ac.uk>
2010-06-06 23:16:34 +0100

    ExcelSplit to StringUtil

--- src/com/pri/util/StringUtils.java
+++ src/com/pri/util/StringUtils.java
@@ -6,8 +6,10 @@
  */
 package com.pri.util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 
 
@@ -396,4 +398,108 @@ public class StringUtils
   return sb!=null?sb.toString():s;
  }
  
+ public static void splitExcelString(String line, String sep, List<String> accum)
+ {
+  int spos;
+  StringBuilder sb=null;
+  
+  while( line.length() > 0 )
+  {
+   
+   if( line.charAt(0) != '"' ) // This cell is not quoted
+   {
+    spos = line.indexOf(sep);
+    
+    if( spos < 0 )
+    {
+     accum.add(line);
+     return;
+    }
+    
+    accum.add(line.substring(0,spos));
+    line = line.substring(spos+sep.length());
+   }
+   else
+   {
+    int qpos;
+    int beg = 1;
+    
+    while( true ) // A quoted cell can contain double quote inside. Let'd build the cell content from parts
+    {
+     qpos = line.indexOf('"',beg);
+     
+     if( qpos == -1 ) // actually this is the erroneous situation - quoted part is not finished by the quotation symbol. 
+     {
+      if( sb != null && sb.length() > 0 )
+      {
+       sb.append(line.substring(beg));
+       accum.add(sb.toString());
+      }
+      else
+       accum.add(line.substring(beg));
+      
+      return;
+     }
+     else if( qpos == line.length()-1 )
+     {
+      if( sb != null && sb.length() > 0 )
+      {
+       sb.append(line.substring(beg,line.length()-1));
+       accum.add(sb.toString());
+      }
+      else
+       accum.add(line.substring(beg,line.length()-1));
+
+      return;
+     }
+     
+     if( line.charAt(qpos+1) == '"' ) // We have found a double quote
+     {
+      if( sb == null )
+       sb = new StringBuilder(200);
+      
+      sb.append(line.substring(beg, qpos+1)); // adding part of the cell to the buffer and continue
+      beg = qpos+2;
+     }
+     else
+     {
+      if( line.startsWith(sep, qpos+1) )
+      {
+       if( sb != null && sb.length() > 0 )
+       {
+        sb.append(line.substring(beg, qpos) );
+        accum.add(sb.toString());
+        sb.setLength(0);
+       }
+       else
+        accum.add(line.substring(beg, qpos));
+        
+       line=line.substring(qpos+sep.length()+1);
+       break;
+      }
+      else // actually this is the erroneous situation - quotation symbol have to be followed by separator or to be doubled . 
+      {
+       if( sb == null )
+        sb = new StringBuilder(200);
+       
+       sb.append(line.substring(beg, qpos+1));
+       beg = qpos+1;
+      }
+     }
+     
+    }
+   }
+  }
+ }
+
+ 
+ public static List<String> splitExcelString(String line, String sep)
+ {
+  List<String> res = new ArrayList<String>(50);
+  
+  splitExcelString(line, sep, res);
+  
+  return res;
+ }
+
 }
