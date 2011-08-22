e1cf72148dfc6b50c4dd708b9d0b97c5c2983c3d
Mike <gostev@ebi.ac.uk>
2011-08-23 00:36:00 +0100

    Empty Set and Map

--- src/com/pri/util/collection/Collections.java
+++ src/com/pri/util/collection/Collections.java
@@ -5,24 +5,41 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.RandomAccess;
+import java.util.Set;
 
 public class Collections
 {
- private static List<Object> EMPTY_LIST = new EmptyList();
+ private static List<Object> EMPTY_COLLECTION = new EmptyCollection();
+ private static Map<Object,Object> EMPTY_MAP = new EmptyMap();
 
  
  
  @SuppressWarnings("unchecked")
  public static final <T> List<T> emptyList()
  {
-  return (List<T>) EMPTY_LIST;
+  return (List<T>) EMPTY_COLLECTION;
  }
+
+ @SuppressWarnings("unchecked")
+ public static final <T> Set<T> emptySet()
+ {
+  return (Set<T>) EMPTY_COLLECTION;
+ }
+
+ 
+ @SuppressWarnings("unchecked")
+ public static final <K,V> Map<K,V> emptyMap()
+ {
+  return (Map<K,V>) EMPTY_MAP;
+ }
+
 
  /**
   * @serial include
   */
- private static class EmptyList implements List<Object>, RandomAccess, Serializable
+ private static class EmptyCollection implements List<Object>, Set<Object>, RandomAccess, Serializable
  {
   private static final long serialVersionUID = 020110716L;
 
@@ -166,4 +183,80 @@ public class Collections
 
  }
 
+ private static class EmptyMap implements Map<Object, Object>
+ {
+
+  @Override
+  public int size()
+  {
+   return 0;
+  }
+
+  @Override
+  public boolean isEmpty()
+  {
+   return true;
+  }
+
+  @Override
+  public boolean containsKey(Object key)
+  {
+   return false;
+  }
+
+  @Override
+  public boolean containsValue(Object value)
+  {
+   return false;
+  }
+
+  @Override
+  public Object get(Object key)
+  {
+   return null;
+  }
+
+  @Override
+  public Object put(Object key, Object value)
+  {
+   throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object remove(Object key)
+  {
+   return null;
+  }
+
+  @Override
+  public void putAll(Map< ? extends Object, ? extends Object> m)
+  {
+   throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void clear()
+  {
+  }
+
+  @Override
+  public Set<Object> keySet()
+  {
+   return emptySet();
+  }
+
+  @Override
+  public Collection<Object> values()
+  {
+   return emptySet();
+  }
+
+  @Override
+  public Set<java.util.Map.Entry<Object, Object>> entrySet()
+  {
+   return emptySet();
+  }
+  
+ }
+ 
 }
