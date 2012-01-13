be9e9b161409ded198db92e045f7afaa4beadb7e
Mike <gostev@ebi.ac.uk>
2012-01-13 14:43:56 +0000

    Mavenized

---
+++ src/test/java/natCmpTest/Test.java
@@ -0,0 +1,63 @@
+package natCmpTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.pri.util.StringUtils;
+
+public class Test
+{
+
+ /**
+  * @param args
+  */
+ public static void main(String[] args)
+ {
+  List<String> lst = new ArrayList<String>()
+  {{
+   add("123");
+   add("AAA");
+   add("X-1");
+   add("X-01");
+   add("X-001");
+   add("X-10");
+   add("X-19");
+   add("X-109");
+   add("X-109-Y");
+   add("X-01-Y");
+   add("");
+   add("X-31-Y11");
+   add("X-01");
+   add("X-31-Y2");
+   add("X-31-Y1");
+   add("X-1X");
+   add("X-31-YZ");
+   add("X-019");
+  }};
+  
+  Collections.sort(lst);
+  
+  for( int i=0; i < lst.size(); i++ )
+   System.out.println(lst.get(i));
+  
+  System.out.println("=====");
+  
+  Collections.sort(lst, new Comparator<String>()
+  {
+
+   @Override
+   public int compare(String str1, String str2)
+   {
+    return StringUtils.naturalCompare(str1, str2);
+   }
+  });
+  
+  for( int i=0; i < lst.size(); i++ )
+   System.out.println(lst.get(i));
+
+
+ }
+
+}
