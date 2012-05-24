69d98a29a77ad86d0aada997cae6a2712571e4b4
Mike <gostev@ebi.ac.uk>
2012-05-24 20:57:24 +0100

    ArrayObjectRecycler

---
+++ src/main/java/com/pri/util/ArrayObjectRecycler.java
@@ -0,0 +1,30 @@
+package com.pri.util;
+
+
+public class ArrayObjectRecycler<T>
+{
+ private Object[] buf;
+ int ptr = 0;
+
+ public ArrayObjectRecycler( int sz )
+ {
+  buf = new Object[sz];
+ }
+ 
+ public synchronized T getObject()
+ {
+  if( ptr == 0 )
+   return null;
+  
+  return (T)buf[--ptr];
+ }
+
+ public synchronized void recycleObject( T obj )
+ {
+  if( ptr == buf.length )
+   return;
+  
+  buf[ptr++] = obj;
+ }
+
+}
