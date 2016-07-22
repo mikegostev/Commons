e725b102ed6882aa998f45493c45fa0dbe807908
Mike <gostev@ebi.ac.uk>
2016-07-22 21:29:16 +0100

    CellStream iface

---
+++ src/main/java/uk/ac/ebi/mg/spreadsheet/CellStream.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.mg.spreadsheet;
+
+import java.io.IOException;
+
+public interface CellStream
+{
+ void addCell(String cont) throws IOException;
+ void addDateCell(long ts) throws IOException;
+ void nextCell() throws IOException;
+ void nextRow() throws IOException;
+ 
+ void start() throws IOException;
+ void finish() throws IOException;
+}
